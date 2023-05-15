da982387b9277f6d995d95f4f61640f1c805353b
Jade Stewart <jadekstewart3@gmail.com>
2023-05-14 18:35:08 -0600

    test: fully complete validation testing

--- spec/models/dream_tag_spec.rb
+++ spec/models/dream_tag_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe DreamTag, type: :model do
   describe "validations" do 
-
+    it { should validate_presence_of :dream_id }
+    it { should validate_presence_of :tag_id }
   end
-  
+
   describe "relationships" do 
     it { should belong_to :dream }
     it { should belong_to :tag }
