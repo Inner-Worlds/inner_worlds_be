5165fd4b95117238cde1081e22ab263af9874b5d
Jade Stewart <jadekstewart3@gmail.com>
2023-05-16 17:51:20 -0600

    feat: create mutation for deleting dream tags

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,5 +5,6 @@ module Types
     field :delete_user, mutation: Mutations::Users::DeleteUser
     field :add_tag, mutation: Mutations::AddTag
     field :delete_dream, mutation: Mutations::DeleteDream
+    field :delete_dream_tag, mutation: Mutations::DeleteDreamTag
   end
 end
