55662aafd45e1244d79b20daa0c93be55e0d6261
Sam Walker <sgwalker327@gmail.com>
2023-05-15 18:26:34 -0600

    Added dependent: destroy to dream_emotions and dream_tags to allow for the deletion of a user

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -7,8 +7,8 @@ class Dream < ApplicationRecord
 
   validates :lucidity, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :user
-  has_many :dream_emotions
+  has_many :dream_emotions, dependent: :destroy
   has_many :emotions, through: :dream_emotions
-  has_many :dream_tags
+  has_many :dream_tags, dependent: :destroy
   has_many :tags, through: :dream_tags
 end
