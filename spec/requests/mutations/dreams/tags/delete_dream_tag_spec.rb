143f2fae2222a41a3d8b96387f59a6d6871e038a
Jade Stewart <jadekstewart3@gmail.com>
2023-05-16 17:50:45 -0600

    test: complete test for deletion of dream tags

---
+++ spec/requests/mutations/dreams/tags/delete_dream_tag_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe DreamTag, type: :request do 
+  describe "Delete dream tag" do 
+   describe "when successful" do 
+    it "deletes a dream tag" do 
+      user = create(:user)
+      dream = create(:dream, user:)
+      tag = create(:tag)
+      dream_tag = create(:dream_tag, tag:, dream:)
+
+      expect(DreamTag.count).to eq(1)
+
+      post "/graphql", params: { query: mutation(dream.id, tag.id)}
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
+
+      deleted_dream_tag = json_response[:data][:deleteDreamTag]
+      
+      expect(DreamTag.count).to eq(0)
+      expect(deleted_dream_tag.keys).to eq([:id])
+      expect(deleted_dream_tag[:id].to_i).to eq(dream.id)
+    end
+   end
+  end
+
+    def mutation(dream_id, tag_id)
+    <<~GQL
+    mutation{
+      deleteDreamTag(input: {dreamId: #{dream_id}, tagId: #{tag_id}})
+      {
+        id
+      }
+    }
+    GQL
+  end
+end
