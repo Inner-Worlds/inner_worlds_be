856cd7ec84c5998daa6acee6dc68e68cfbbab9f8
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-05-16 16:07:43 -0600

    Add emotion to mutation type

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :delete_user, mutation: Mutations::Users::DeleteUser
     field :add_tag, mutation: Mutations::AddTag
+    field :add_emotion, mutation: Mutations::AddEmotion
     field :createDream, mutation: Mutations::CreateDream
     field :deleteDream, mutation: Mutations::DeleteDream
   end
