7e0ed4efd7185a9bde12682beb08cdfacbdbf32b
Weston Sandfort <sandfortw@gmail.com>
2023-05-15 19:47:41 -0600

    modify dream tag type and mutation type for AddTag

--- app/graphql/types/dream_tag_type.rb
+++ app/graphql/types/dream_tag_type.rb
@@ -3,8 +3,8 @@
 module Types
   class DreamTagType < Types::BaseObject
     field :id, ID, null: false
-    field :dream_id, Integer, null: false
-    field :tag_id, Integer, null: false
+    field :dream, Types::DreamType, null: false
+    field :tag, Types::TagType, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
