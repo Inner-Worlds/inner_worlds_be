e28c9a7d585e85ac7a0b95196e5389b46957a836
Bobby Luly <bobbyluly@gmail.com>
2023-05-17 17:45:40 -0600

    Test: add method to get default tags
    Co-authored-by: Kassandra Leyba <kassandra.leyba@gmail.com>

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Tag, type: :model do
     it { should have_many :dream_tags }
     it { should have_many(:dreams).through(:dream_tags) }
   end
+
+  describe 'class methods' do
+    describe '.default_tags' do
+      it 'returns an array of default tags' do
+        happy = Tag.create(name: 'happy', default: true)
+        sad = Tag.create(name: 'sad', default: true)
+        angry = Tag.create(name: 'angry', default: true)
+
+        melancholy = Tag.create(name: 'melancholy')
+        somber = Tag.create(name: 'somber')
+
+        expect(Tag.default_tags).to eq([happy, sad, angry])
+        expect(Tag.default_tags).to_not include(melancholy, somber)
+      end
+    end
+  end
 end
