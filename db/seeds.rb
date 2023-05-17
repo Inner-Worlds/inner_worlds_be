ba2ba14044d918b3b80b9ae669a79735658baf46
Weston Sandfort <sandfortw@gmail.com>
2023-05-17 11:30:08 -0600

    truncate the database when running rails db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,7 @@
-DreamEmotion.delete_all
-DreamTag.delete_all
-Tag.delete_all
-Emotion.delete_all
-Dream.delete_all
-User.delete_all
+ActiveRecord::Base.connection.tables.each do |table|
+  ActiveRecord::Base.connection.execute("TRUNCATE #{table} RESTART IDENTITY CASCADE")
+end
 
-
-
-# frozen_string_literal: true
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 100.times do
   user = User.create(name: Faker::TvShows::VentureBros.character, email: Faker::Internet.email)
   3.times do
