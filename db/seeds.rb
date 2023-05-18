ec2cebddc33630c4981c89a8c73c679fcd730ebb
Weston Sandfort <sandfortw@gmail.com>
2023-05-18 08:58:26 -0600

    fix issue with deleting tables in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 ActiveRecord::Base.connection.tables.each do |table|
+  next if table == 'schema_migrations'
   ActiveRecord::Base.connection.execute("TRUNCATE #{table} RESTART IDENTITY CASCADE")
 end
 
