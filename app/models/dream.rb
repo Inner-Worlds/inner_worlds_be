e7cdeb961e3c1f8778f503b0ca389a0cf09f4c87
Jade Stewart <jadekstewart3@gmail.com>
2023-05-19 14:38:12 -0600

    chore: remove commented out text

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -5,20 +5,11 @@ class Dream < ApplicationRecord
                         :title,
                         :dream_date
 
-  # validates :dream_date, format: /\d{2}\/\d{2}\/\d{4}/
-  # before_save :ensure_datetime_object
   validates :lucidity, numericality: { greater_than_or_equal_to: 0 }
+  
   belongs_to :user
   has_many :dream_emotions, dependent: :destroy
   has_many :emotions, through: :dream_emotions
   has_many :dream_tags, dependent: :destroy
   has_many :tags, through: :dream_tags
-
-  # private 
-  # def ensure_datetime_object
-  #   require 'pry'; binding.pry
-  #   if self.dream_date.class == String
-  #     DateTime.strptime(self.dream_date, '%m/%d/%Y')
-  #   end
-  # end
 end
