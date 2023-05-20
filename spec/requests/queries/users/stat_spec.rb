b987955b51ce683146026eaf59b90f0f77540e12
Bobby Luly <bobbyluly@gmail.com>
2023-05-20 15:10:13 -0600

    Test: finish stats request test for user's top_5_tags

--- spec/requests/queries/users/stat_spec.rb
+++ spec/requests/queries/users/stat_spec.rb
@@ -2,23 +2,78 @@
 
 require 'rails_helper'
 
-RSpec.describe "Getting a User\'s Stats", type: :request do
-  describe 'when successful' do
-    xit 'successfully returns one user\'s stats' do
-      #TO DO
+RSpec.describe User, type: :request do
+  describe "Getting a User's Stats" do
+    context 'when successful' do
+      before do
+        @user = create(:user)
+        @dream1 = create(:dream, user: @user)
+        @dream2 = create(:dream, user: @user)
+        @dream3 = create(:dream, user: @user)
+        @dream4 = create(:dream, user: @user)
+
+        @tag1 = create(:tag, name: 'happy')
+        @tag2 = create(:tag, name: 'sad')
+        @tag3 = create(:tag, name: 'angry')
+        @tag4 = create(:tag, name: 'melancholy')
+        @tag5 = create(:tag, name: 'somber')
+        @tag6 = create(:tag, name: 'joyful')
+        @tag7 = create(:tag, name: 'anxious')
+
+        DreamTag.create(dream: @dream1, tag: @tag1)
+        DreamTag.create(dream: @dream1, tag: @tag2)
+        DreamTag.create(dream: @dream1, tag: @tag3)
+
+        DreamTag.create(dream: @dream2, tag: @tag1)
+        DreamTag.create(dream: @dream2, tag: @tag5)
+
+        DreamTag.create(dream: @dream3, tag: @tag1)
+        DreamTag.create(dream: @dream3, tag: @tag2)
+        DreamTag.create(dream: @dream3, tag: @tag4)
+
+        DreamTag.create(dream: @dream4, tag: @tag1)
+        DreamTag.create(dream: @dream4, tag: @tag2)
+        DreamTag.create(dream: @dream4, tag: @tag3)
+      end
+
+      it "successfully returns a user's top 5 tag stats" do
+        post '/graphql', params: { query: query_user_stats(@user.id) }
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        user_data = user_response[:data][:user][:stats]
+
+        expect(user_data).to have_key(:top5Tags)
+        expect(user_data[:top5Tags]).to be_an(Array)
+        expect(user_data[:top5Tags].count).to eq(5)
+
+        user_data[:top5Tags].each do |tag|
+          expect(tag).to have_key(:name)
+          expect(tag[:name]).to be_a(String)
+
+          expect(tag).to have_key(:frequency)
+          expect(tag[:frequency]).to be_an(Integer)
+
+          expect(tag).to have_key(:percent)
+          expect(tag[:percent]).to be_a(Float)
+        end
+
+        expect(user_data[:top5Tags].first[:name]).to eq(@tag1.name)
+        expect(user_data[:top5Tags].first[:frequency]).to eq(4)
+        expect(user_data[:top5Tags].first[:percent]).to eq(100.0)
+      end
     end
   end
 
-  describe 'when not successful' do
+  context 'when not successful' do
     xit 'some error message is returned' do
       #TO DO
     end
   end
 
-  def query_user_stats(id)
+  def query_user_stats(user_id)
     <<~GQL
       query{
-        user(id: #{id}){
+        user(id: #{user_id}){
           stats{
             currentStreak
             longestStreak
