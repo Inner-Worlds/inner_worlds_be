89eac02729b87bb55ae75b9bd9675a81da1e2fa1
Bobby Luly <bobbyluly@gmail.com>
2023-05-23 15:53:55 -0600

    Add commented out code block for mailers in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,22 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  #--------------------------------------------------->Needed for mailers in production
+  # config.action_mailer.delivery_method = :smtp
+  # host = 'boiling-hollows-45055.herokuapp.com' #replace with your own url
+  # config.action_mailer.default_url_options = { host: host }
+
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.gmail.com",
+  #   :port                 => 587,
+  #   :user_name            => ENV["INNER_WORLDS_USERNAME"],
+  #   :password             => ENV["INNER_WORLDS_PASSWORD"],
+  #   :authentication       => "plain",
+  #   :enable_starttls_auto => true
+  # }
+  #--------------------------------------------------->
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -85,4 +101,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
 end
