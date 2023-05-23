0b17a0feac1ae9520a7086b7e83f31f6f1f13476
Weston Sandfort <sandfortw@gmail.com>
2023-05-23 15:04:20 -0600

    create test for tag facade
    Co-authored-by: NAME <sgwalker327@gmail.com>
    Co-authored-by: NAME <kassandra.leyba@gmail.com>

---
+++ spec/facades/tag_facade_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe TagFacade do 
+
+  describe '#generate_tags' do
+    it 'should generate tags from a user description and given tags' do
+      VCR.use_cassette("facades/tag_facade") do
+        user = create(:user)
+        dream = create(:dream, user: user, description: "how much is that doggie in the window? The one with the waggily tail. How much is that doggie in the window? I do hope that doggie's for sale.")
+        user_tags = ['cheese', 'apple', 'orange']
+        facade = TagFacade.new(dream, user_tags)
+        facade.generate_tags
+        
+        expect(user.dreams.first.tags.size).to eq(7)
+        expect(user.dreams.first.tags.pluck(:name)).to eq(["cheese", "apple", "orange", "window", "tail", "hope", "door"])
+      end
+    end
+  end
+end
