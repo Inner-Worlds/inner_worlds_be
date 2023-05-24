e9c26593d08880e4c45d9b782934a145033dca3e
Bobby Luly <bobbyluly@gmail.com>
2023-05-23 21:15:16 -0600

    Delete unnecessary sidekiq scheduler stuff
    Co-authored-by: Jade Stewart <jadekstewart3@gmail.com>

--- config/environment.rb
+++ config/environment.rb
@@ -2,16 +2,6 @@
 
 # Load the Rails application.
 require_relative 'application'
-require 'sidekiq-scheduler'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Sidekiq::Scheduler.enabled = true
-Sidekiq.schedule = {
-  stat_email: {
-    every: '1m',
-    # cron: '0 0 * * 0',
-    class: 'StatSenderWorker'
-    }
-}
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,3 @@
-require "sidekiq-scheduler"
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379/0' }
