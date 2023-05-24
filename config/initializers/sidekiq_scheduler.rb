24065c30b9b8e228dff378d68bb0fddd312dcd24
Bobby Luly <bobbyluly@gmail.com>
2023-05-24 17:40:12 -0600

    Comment out test linke for weekly email

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -3,8 +3,8 @@ require 'sidekiq-scheduler'
 Sidekiq::Scheduler.enabled = true
 Sidekiq.schedule = {
   stat_email: {
-    every: '1m',
-    # cron: '0 0 * * 0',
+    # every: '1m', <-- use this for testing
+    cron: '0 0 * * 0',
     class: 'StatSenderWorker'
   }
 }
