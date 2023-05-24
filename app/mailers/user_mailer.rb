3f3a2a01fe4b29d319ee4450e961d24857897c79
Bobby Luly <bobbyluly@gmail.com>
2023-05-23 21:16:47 -0600

    Feat: create email template and view
    Co-authored-by: Jade Stewart <jadekstewart3@gmail.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def stat_email(user)
     @user = user
-    mail(to: @user.email, subject: "Greetings, Cadet! Here are your out of this world dream stats!" )
+    mail(to: @user.email, subject: "Inner Worlds - Your out of this world dream stats!" )
   end
 end
