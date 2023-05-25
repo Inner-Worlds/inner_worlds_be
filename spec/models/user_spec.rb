0f8ba533de69012a87553d557d9015911abe7c86
Weston Sandfort <sandfortw@gmail.com>
2023-05-25 10:18:25 -0600

    Refactor average weekly lucidity test
    Co-authored-by: <sgwalker327@gmail.com>
    Co-authored-by: <kassandra.leyba@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -102,14 +102,9 @@ RSpec.describe User, type: :model do
         dream1 = create(:dream, lucidity: 3, user: user, dream_date: Date.today + 1)
         dream2 = create(:dream, lucidity: 2, user: user, dream_date: Date.today + 2)
         dream3 = create(:dream, lucidity: 4, user: user, dream_date: Date.today + 3)
-        
-        week_start_date = Date.today.beginning_of_week(:sunday)
-        week_end_date = week_start_date + 6
-    
-        average = (dream1.lucidity + dream2.lucidity + dream3.lucidity).to_f / 3
 
         expect(user.average_lucidity).to be_a(Float)
-        expect(user.average_lucidity).to eq(average.round(2))
+        expect(user.average_lucidity).to eq(3.0)
       end
     end
   
