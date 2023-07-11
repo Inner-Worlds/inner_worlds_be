8593795ad6122c5625314884bc2206fb0d4390d7
Weston Sandfort <sandfortw@gmail.com>
2023-07-11 09:43:56 -0600

    Remove unnecessary env variable, and replace with plain text

--- app/services/topic_service.rb
+++ app/services/topic_service.rb
@@ -8,7 +8,7 @@ class TopicService
   def get_topics
     response = conn.get do |req|
       req.headers['X-RapidAPI-Key'] = ENV['TOPICS_KEY']
-      req.headers['X-RapidAPI-Host'] = ENV['TOPICS_HOST']
+      req.headers['X-RapidAPI-Host'] = 'twinword-topic-tagging.p.rapidapi.com'
       req.params['text'] = @payload
     end
     json = JSON.parse(response.body, symbolize_names: true)
