c45e0bc655ad837aa01e6a7164d1030e1d54e964
Weston Sandfort <sandfortw@gmail.com>
2023-09-09 10:53:43 -0600

    Configure hooks

---
+++ .platform/hooks/prebuild/01_config_database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'Add database.yml'
+
+EB_APP_STAGING_DIR=$(sudo /opt/elasticbeanstalk/bin/get-config platformconfig -k AppStagingDir)
+cd $EB_APP_STAGING_DIR
+sudo cp ./config/database.yml.eb ./config/database.yml
